c170d6af23f381ed4a39011fc5593ce74dd53075
Hung Mai <hwngmvi@gmail.com>
2022-08-29 11:00:32 +0700

    commit 2

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -9,13 +9,60 @@ import { Heading1, Heading2, Heading3 } from "../components/Heading";
 import { Label, TextField, Input } from "../components/TextField";
 import { Button } from "../components/Button";
 import { Table, Tr, Td, Th, Thead, Tbody } from "../components/Table";
+//redux
+import { connect } from "react-redux";
 class ToDoList extends Component {
+  // Hàm rendertasktodo()
+  // filter() từng task nếu task.done == true thì cho vào mảng mới
+  // map() mảng mới đó rồi return các giá trị Tr theo mẫu
+  renderTaskToDo = () => {
+    return this.props.taskList
+      .filter((task) => task.done == true)
+      .map((task, index) => {
+        return (
+          <Tr key={index}>
+            <Th className='text-left'> {task.taskName} </Th>
+            <Th className='text-right'>
+              {" "}
+              <Button className='ml-2'>
+                <i className='fa fa-edit'></i>
+              </Button>{" "}
+              <Button className='ml-2'>
+                <i className='fa fa-check'></i>
+              </Button>{" "}
+              <Button className='ml-2'>
+                <i className='fa fa-trash'></i>
+              </Button>{" "}
+            </Th>
+          </Tr>
+        );
+      });
+  };
+  // Hàm rendertaskdone()
+  // filter() từng task nếu task.done == false thì cho vào mảng mới
+  // map() mảng mới đó rồi return các giá trị Tr theo mẫu
+  renderTaskDone = () => {
+    return this.props.taskList
+      .filter((task) => !task.done)
+      .map((task, index) => {
+        return (
+          <Tr key={index}>
+            <Th className='text-left'> {task.taskName} </Th>
+            <Th className='text-right'>
+              <Button className='ml-2'>
+                <i className='fa fa-trash'></i>
+              </Button>{" "}
+            </Th>
+          </Tr>
+        );
+      });
+  };
   render() {
     return (
-      <ThemeProvider theme={ToDoListPrimaryTheme}>
+      <ThemeProvider theme={this.props.themeToDoList}>
         <div>
           {" "}
-          <Container className='w-50'>
+          <Container className='w-75'>
             <Dropdown>
               <option>Dark Theme</option>
               <option>Light Theme</option>
@@ -34,44 +81,12 @@ class ToDoList extends Component {
             <hr />
             <Heading3 className='text-left'>Task to do</Heading3>
             <Table>
-              <Thead>
-                <Tr>
-                  <Th className='text-left'> Task name </Th>
-                  <Th className='text-right'>
-                    {" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-edit'></i>
-                    </Button>{" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-check'></i>
-                    </Button>{" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-trash'></i>
-                    </Button>{" "}
-                  </Th>
-                </Tr>
-              </Thead>
+              <Thead>{this.renderTaskToDo()}</Thead>
             </Table>
             <hr />
             <Heading3 className='text-left'>Task Complete</Heading3>
             <Table>
-              <Thead>
-                <Tr>
-                  <Th className='text-left'> Task name </Th>
-                  <Th className='text-right'>
-                    {" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-edit'></i>
-                    </Button>{" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-check'></i>
-                    </Button>{" "}
-                    <Button className='ml-2'>
-                      <i className='fa fa-trash'></i>
-                    </Button>{" "}
-                  </Th>
-                </Tr>
-              </Thead>
+              <Thead>{this.renderTaskDone()}</Thead>
             </Table>
           </Container>
         </div>
@@ -79,5 +94,10 @@ class ToDoList extends Component {
     );
   }
 }
-
-export default ToDoList;
+const mapStatetoProps = (state) => {
+  return {
+    themeToDoList: state.ToDoListReducer.themeToDoList,
+    taskList: state.ToDoListReducer.taskList,
+  };
+};
+export default connect(mapStatetoProps)(ToDoList);
--- src/redux/reducers/ToDoListReducer.js
+++ src/redux/reducers/ToDoListReducer.js
@@ -1,8 +1,13 @@
 import { ToDoListDarkTheme } from "../../themes/ToDoListDarkTheme";
+import { ToDoListLightTheme } from "../../themes/ToDoListLightTheme";
+import { ToDoListPrimaryTheme } from "../../themes/ToDoListPrimaryTheme";
 
 const initialState = {
-  themeToDoList: ToDoListDarkTheme,
-  taskList: [],
+  themeToDoList: ToDoListLightTheme,
+  taskList: [
+    { id: "task-1", taskName: "Ăn cơm", done: true },
+    { id: "task-2", taskName: "Lau nhà", done: false },
+  ],
 };
 
 export default (state = initialState, { action }) => {
